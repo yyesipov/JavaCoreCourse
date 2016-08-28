7415332602761c4ee73ab4bed1bf2ba0ced7f9db
yyesipoov <yyesipov@gmail.com>
2016-08-29 00:02:50 +0300

    Add task1 from homework (module3)

--- src/module3/homework/task1/Bird.java
+++ src/module3/homework/task1/Bird.java
@@ -5,4 +5,8 @@ public class Bird {
     void fly() {
         System.out.println("I am flying");
     }
+
+    void sing() {
+        System.out.println("I am singing");
+    }
 }
--- src/module3/homework/task1/Solution.java
+++ src/module3/homework/task1/Solution.java
@@ -2,6 +2,13 @@ package module3.homework.task1;
 
 public class Solution {
     public static void main(String[] args){
+        Animal walk = new Animal();
+        walk.walk();
+        Bird fly = new Bird();
+        fly.fly();
+        Bird sing = new Bird();
+        sing.sing();
+        System.out.println("I am Bird");
 
     }
 }
